feafabeb2f63d25fd1af9568ee62f24396dc4021
siebren014 <74716099+siebren014@users.noreply.github.com>
2022-05-11 14:45:41 +0200

    combine

--- Calibration/calibration_method.cpp
+++ Calibration/calibration_method.cpp
@@ -29,30 +29,6 @@ bool Calibration::calibration(
         Matrix33& R,               /// output: the 3x3 rotation matrix encoding camera orientation.
         Vector3D& t)               /// output：a 3D vector encoding camera translation.
 {
-    // TODO: the above code just demonstrates some useful data structures and APIs. Please remove all above code in your
-    //       final submission.
-
-    //--------------------------------------------------------------------------------------------------------------
-    // implementation starts ...
-
-//
-//    for (const auto&point: points_3d){
-//        std::cout<<point<<std::endl;
-//    }
-
-//    std::cout << "\n[Liangliang]:\n"
-//                 "\tThe input parameters of this function are:\n"
-//                 "\t\t- points_3d: An array of 3D points (input to this function)\n"
-//                 "\t\t- points_2d: An array of 2D image points (input to this function)\n"
-//                 "\tThis function must return either 'true' on success or 'false' otherwise. On success, the camera\n"
-//                 "\tparameters are returned by the following variables:\n"
-//                 "\t\t- fx and fy: the focal lengths (in our slides, we use 'alpha' and 'beta')\n"
-//                 "\t\t- cx and cy: the principal point (in our slides, we use 'u0' and 'v0')\n"
-//                 "\t\t- skew:      the skew factor ('-alpha * cot_theta')\n"
-//                 "\t\t- R:         the 3x3 rotation matrix encoding camera orientation\n"
-//                 "\t\t- t:         a 3D vector encoding camera location.\n"
-//                 "\tIMPORTANT: don't forget to write your recovered parameters to the above variables." << std::endl;
-
 
 
     // TODO: check if input is valid (e.g., number of correspondences >= 6, sizes of 2D/3D points must match)
@@ -76,10 +52,6 @@ bool Calibration::calibration(
     for (const auto & point: points_3d_and_2d ){
         std::cout<<point<<std::endl;
     }
-
-
-    // TODO: construct the P matrix (so P * m = 0).
-
 
     Matrix mat = Matrix(points_3d.size(), 5, all_point);
 
@@ -143,52 +115,104 @@ bool Calibration::calibration(
     std::cout<<"we here"<<std::endl;
     std::cout << N << "\n" << std::endl;
 
-    std::cout<<std::endl<<"M matric"<< std::endl;
+    std::cout<<std::endl<<"M matrix"<< std::endl;
     std::cout<<M<<std::endl;
 
-    for (int i = 0; i < M.size(); i++){
+    for (int i = 0; i < N.size(); i++){
         std::cout<<"test"<<std::endl;
-        std::cout<<M[i]<<std::endl;
-        if (M[i]> 1){
-            throw std::invalid_argument( "M too big input" );
+        std::cout<<N[i]<<std::endl;
+        if (N[i]> 0.0001){
+            throw std::invalid_argument( "input data too noisy" );
         }
     }
 
-    //add check to see if close to zero
+    Matrix34 MM = Matrix(3,4,0.0);
+    MM[0][0]=M[0];
+    MM[0][1]=M[1];
+    MM[0][2]=M[2];
+    MM[0][3]=M[3];
+    MM[1][0]=M[4];
+    MM[1][1]=M[5];
+    MM[1][2]=M[6];
+    MM[1][3]=M[7];
+    MM[2][0]=M[8];
+    MM[2][1]=M[9];
+    MM[2][2]=M[10];
+    MM[2][3]=M[11];
 
+    std::cout<<"MM matrix"<<MM<<std::endl;
 
+    Vector a1 = Vector3D(MM[0][0], MM[0][1], MM[0][2]);
+    Vector a2 = Vector3D(MM[1][0], MM[1][1], MM[1][2]);
+    Vector a3 = Vector3D(MM[2][0], MM[2][1], MM[2][2]);
+    Vector b = Vector3D(MM[0][3], MM[1][3], MM[2][3]);
+    std::cout << "a1 "<< a1 << "\n" << std::endl;
+    std::cout << "a2 "<< a2 << "\n" << std::endl;
+    std::cout << "a3 "<< a3 << "\n" << std::endl;
+    std::cout << "b "<< b << "\n" << std::endl;
 
-    // TODO: solve for M (the whole projection matrix, i.e., M = K * [R, t]) using SVD decomposition.
-    //   Optional: you can check if your M is correct by applying M on the 3D points. If correct, the projected point
-    //             should be very close to your input images points.
-
-
-
-    // TODO: extract intrinsic parameters from M.
-
-    Vector a1 = Vector(M[1][1], M[1][2]);
-    Vector a2 = Vector(M[2][1], M[2][2]);
-    Vector a3 = Vector(M[3][1], M[3][2]);
 
     double ro = 0.0;
     double u0 = 0.0;
     double v0 = 0.0;
-    double costheta = 0.0;
+    double alpha = 0.0;
+    double beta = 0.0;
     ro = 1.0/(a3.norm());
+    std::cout << "ro "<< ro << "\n" << std::endl;
+
     u0 = pow(ro,2)*dot(a1,a3);
     v0 = pow(ro,2)*dot(a2,a3);
-    costheta = -(dot(cross(a1,a3),(cross(a2,a3)))/(dot(norm(cross(a1,a3))),norm(cross(a2,a3))));
-    alpha = pow(ro,2)
+    std::cout << "u0 "<< u0 << "\n" << std::endl;
+    std::cout << "v0 "<< v0 << "\n" << std::endl;
+
+    double costheta = -((dot(cross(a1,a3),(cross(a2,a3))))/(((cross(a1,a3)).norm())*((cross(a2,a3)).norm())));
+    double theta = acos(costheta);
+    double sintheta = sin(theta);
+    alpha = pow(ro,2)*norm(cross(a1,a3))*sintheta;
+    beta = pow(ro,2)*norm(cross(a1,a3))*sintheta;
+    std::cout << "costheta "<< costheta << "\n" << std::endl;
+    std::cout << "theta "<< theta << "\n" << std::endl;
+    std::cout << "sintheta "<< sintheta << "\n" << std::endl;
+
+    Matrix33 K = (3,3,0.0);
+    K[0][0]= alpha;
+    K[0][1]= -alpha*(costheta*sintheta);
+    K[0][2]= u0;
+    K[1][0]= 0;
+    K[1][1]= beta/sintheta;
+    K[1][2]= v0;
+    K[2][0]= 0;
+    K[2][1]= 0;
+    K[2][2]= 1;
 
 
+    std::cout<<"K matrix"<<K<<std::endl;
+    std::cout<<"ro"<<ro<<std::endl;
+    std::cout<<"u0"<<u0<<std::endl;
+    std::cout<<"v0"<<v0<<std::endl;
+    std::cout<<"costheta"<<costheta<<std::endl;
+    std::cout<<"alpha"<<alpha<<std::endl;
+    std::cout<<"beta"<<beta<<std::endl;
 
+    //add check to see if close to zero
 
-    // TODO: extract extrinsic parameters from M.
+    Vector3D r1 = (cross(a2,a3))/(norm(cross(a2,a1)));
+    std::cout<<"r1 "<<r1<<std::endl;
+    Vector3D r3 = ro*(a3);
+    Vector3D r2 = cross(r3,r1);
+    std::cout<<"r2 "<<r2<<std::endl;
+    std::cout<<"r3 "<<r3<<std::endl;
+    //Vector r4 = (9,)
 
-    std::cout << "\n\tTODO: After you implement this function, please return 'true' - this will trigger the viewer to\n"
-                 "\t\tupdate the rendering using your recovered camera parameters. This can help you to visually check\n"
-                 "\t\tif your calibration is successful or not.\n\n" << std::flush;
-    return false;
+    R.set_row(0,{r1[0], r1[1], r1[2]});
+    R.set_row(1,{r2[0], r2[1], r2[2]});
+    R.set_row(2,{r3[0], r3[1], r3[2]});
+    std::cout<<"checkkk "<<R<<std::endl;
+
+    Vector3D transl = ro* mult(inverse(K),b);
+    std::cout<<"checkkktr "<<transl<<std::endl;
+
+    return true;
 }
 
 
